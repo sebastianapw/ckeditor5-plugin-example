1a752640766049395793aa09ae27c1455cbb3c07
sebastianapw <19513409+sebastianapw@users.noreply.github.com>
2024-09-03 15:11:42 +0200

    initial commit and setup of angular + ckeditor5 (new installation method)

---
+++ ckeditor-angular-custom-plugin-example/src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import {
+  ClassicEditor,
+  Bold,
+  Essentials,
+  Heading,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  List,
+  MediaEmbed,
+  Paragraph,
+  Table,
+  Undo,
+} from 'ckeditor5';
+import 'ckeditor5/ckeditor5.css';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CKEditorModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'ckeditor-angular-custom-plugin-example';
+
+  public Editor = ClassicEditor;
+  public config = {
+    toolbar: [
+      'undo',
+      'redo',
+      '|',
+      'heading',
+      '|',
+      'bold',
+      'italic',
+      '|',
+      'link',
+      'insertTable',
+      'mediaEmbed',
+      '|',
+      'bulletedList',
+      'numberedList',
+      'indent',
+      'outdent',
+    ],
+    plugins: [
+      Bold,
+      Essentials,
+      Heading,
+      Indent,
+      IndentBlock,
+      Italic,
+      Link,
+      List,
+      MediaEmbed,
+      Paragraph,
+      Table,
+      Undo,
+    ],
+  };
+}
